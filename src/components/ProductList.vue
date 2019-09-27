8365b9baf4e037f0c9363f14e3f7eb5a6a02a493
Диас <standlight@yandex.ru>
2019-09-28 00:44:55 +0300

    Добавил запрос продуктов

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="card-body">
     <div class="products-list row align-items-center justify-content-around no-gutters">
-      <div class="card mb-3" style="width: 18rem;" v-for="(product, index) in products"  v-bind:key="index">
+      <div
+        class="card mb-3"
+        style="width: 18rem;"
+        v-for="(product, index) in products"
+        v-bind:key="index"
+      >
         <img
-          src="https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg"
+          src="./../assets/left-boot.jpg"
           class="card-img-top"
           alt="..."
         />
         <div class="card-body">
           <h5 class="card-title">{{product.name}}</h5>
           <p class="card-text">{{product.description}}</p>
-          <p class="card-text text-muted">{{product.description}}</p>
+          <p class="card-text text-muted">{{product.price}} rub</p>
         </div>
       </div>
     </div>
@@ -18,43 +23,18 @@
 </template>
 
 <script>
+import { http } from "./../helpers/http";
 export default {
   name: "ProductList",
   data: function() {
     return {
-      products: [
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        },
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        },
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        },
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        },
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        },
-        {
-          name: "Навзание продукта 1",
-          description: "Описание продукта",
-          price: "Цена продукта"
-        }
-      ]
+      products: []
     };
+  },
+  mounted: function() {
+    http.get("/product/list").then(response => {
+      this.products = response.data
+    });
   }
 };
 </script>
